0cdc49216d114364a30d243eef25d3320d4caa07
WAYNESON CLYV SANTOS <waynesonclyv.santos.21@usjr.edu.ph>
2023-09-27 01:30:30 +0800

    Completed Backend of Triangle. Work on Frontend of Triangle next.

--- ViewModels/TriangleViewModel.cs
+++ ViewModels/TriangleViewModel.cs
@@ -10,27 +10,70 @@ namespace ShapeCalculator.ViewModels
 {
     public partial class TriangleViewModel : ObservableObject
     {
+        //Area
         [ObservableProperty]
-        private decimal _AreaTriangle;
-
+        private double _AreaTriangle = 0;
         [ObservableProperty]
-        private decimal _AreaTriangleBase = 0;
-
+        private double _AreaTriangleBase = 0;
         [ObservableProperty]
-        private decimal _AreaTriangleHeight = 0;
+        private double _AreaTriangleHeight = 0;
 
         [RelayCommand]
-        private void CalculateTriangleArea()
+        private void CalculateAreaTriangle()
         {
-            AreaTriangle = ((decimal)0.5 * AreaTriangleBase * AreaTriangleHeight);
+            AreaTriangle = (0.5 * AreaTriangleBase * AreaTriangleHeight);
         }
-
         [RelayCommand]
         private void ClearTriangleArea()
         {
             AreaTriangle = 0;
             AreaTriangleBase = 0;
             AreaTriangleHeight = 0;
+        }
+
+        //Perimeter of the Triangle
+        [ObservableProperty]
+        private double _PerimeterTriangle = 0;
+        [ObservableProperty]
+        private double _PerimeterTriangleSide1 = 0;
+        [ObservableProperty]
+        private double _PerimeterTriangleBase = 0;
+        [ObservableProperty]
+        private double _PerimeterTriangleSide2 = 0;
+
+        [RelayCommand]
+        private void CalculatePerimeterTriangle() 
+        {
+            PerimeterTriangle = PerimeterTriangleSide1 + PerimeterTriangleBase + PerimeterTriangleSide2;
+        }
+        [RelayCommand]
+        private void ClearPerimeterTriangle()
+        {
+            PerimeterTriangle = 0;
+            PerimeterTriangleSide1 = 0;
+            PerimeterTriangleBase = 0;
+            PerimeterTriangleSide2 = 0;
+        }
+
+        //Volume of the Triangle
+        [ObservableProperty]
+        private double _VolumeTriangle = 0;
+        [ObservableProperty]
+        private double _VolumeTriangleRadius = 0;
+        [ObservableProperty]
+        private double _VolumeTriangleHeight = 0;
+
+        [RelayCommand]
+        private void CalculateVolumeTriangle()
+        {
+            VolumeTriangle = ((1/3) * Math.PI * Math.Pow(VolumeTriangleRadius, 2) * VolumeTriangleHeight);
+        }
+        [RelayCommand]
+        private void ClearVolumeTriangle()
+        {
+            VolumeTriangle = 0;
+            VolumeTriangleRadius = 0;
+            VolumeTriangleHeight = 0;
         }
     }
 }
