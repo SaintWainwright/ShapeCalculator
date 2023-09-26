64574b6e7d1131fbc76f205095f438707f7be269
WAYNESON CLYV SANTOS <waynesonclyv.santos.21@usjr.edu.ph>
2023-09-26 23:31:36 +0800

    Did the binding for Triangle Area. Work on the rest next.

--- ShapePages/Triangle.xaml.cs
+++ ShapePages/Triangle.xaml.cs
@@ -1,5 +1,6 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using ShapeCalculator.ViewModels;
 
 namespace ShapeCalculator;
 
@@ -8,35 +9,6 @@ public partial class Triangle : ContentPage
 	public Triangle()
 	{
 		InitializeComponent();
+		BindingContext = new TriangleViewModel();
 	}
-
-	decimal areaTriangleBase = 0;
-	decimal areaTriangleHeight = 0;
-	decimal areaTriangle;
-
-	public decimal AreaTriangle
-	{
-		get { return areaTriangleBase; }
-		set
-		{
-			areaTriangle = value;
-			OnPropertyChanged();
-		}
-	}
-	public void InitializeAreaTriangle()
-	{
-		areaTriangleBase = Int32.Parse(txtEntryEdge1.Text);
-		areaTriangleHeight = Int32.Parse(txtEntryEdge2.Text);
-	}
-
-	public void CalculateArea()
-	{
-        InitializeAreaTriangle();
-		AreaTriangle = ((decimal) 0.5 * areaTriangleBase * areaTriangleHeight);
-	}
-
-    private void Button_Clicked(object sender, EventArgs e)
-    {
-		CalculateArea();
-    }
 }
---
+++ ViewModels/TriangleViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeCalculator.ViewModels
+{
+    public partial class TriangleViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private decimal _AreaTriangle;
+
+        [ObservableProperty]
+        private decimal _AreaTriangleBase = 0;
+
+        [ObservableProperty]
+        private decimal _AreaTriangleHeight = 0;
+
+        [RelayCommand]
+        private void CalculateArea()
+        {
+            AreaTriangle = ((decimal)0.5 * AreaTriangleBase * AreaTriangleHeight);
+        }
+    }
+}
