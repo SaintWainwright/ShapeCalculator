eb1dc6369b448cc173871bffcc3eccdd0e19165a
WAYNESON CLYV SANTOS <waynesonclyv.santos.21@usjr.edu.ph>
2023-09-26 16:59:30 +0800

    adnshbfg

--- ShapePages/Triangle.xaml.cs
+++ ShapePages/Triangle.xaml.cs
@@ -9,4 +9,34 @@ public partial class Triangle : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	decimal areaTriangleBase = 0;
+	decimal areaTriangleHeight = 0;
+	decimal areaTriangle;
+
+	public decimal AreaTriangle
+	{
+		get { return areaTriangleBase; }
+		set
+		{
+			areaTriangle = value;
+			OnPropertyChanged();
+		}
+	}
+	public void InitializeAreaTriangle()
+	{
+		areaTriangleBase = Int32.Parse(txtEntryEdge1.Text);
+		areaTriangleHeight = Int32.Parse(txtEntryEdge2.Text);
+	}
+
+	public void CalculateArea()
+	{
+        InitializeAreaTriangle();
+		AreaTriangle = ((decimal) 0.5 * areaTriangleBase * areaTriangleHeight);
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		CalculateArea();
+    }
 }
